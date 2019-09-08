f8dc6a42dc6368fbd98779e467f94f4cc42d3357
aj518480 <allisoncordovez@gmail.com>
2019-09-08 15:32:07 -0400

    Created buttons on Jumbotron for the options of User to choose for the future populated questions. Seperated them in row in columns and used BS for positioning. On JS made variables of Choices for the future to link to buttons on html. Also created 10 varibles of strings for each question

---
+++ assets/javascript/app.js
@@ -0,0 +1,27 @@
+$( document ).ready();{
+//Make vars to put in the buttons, to make the user choose different answers
+var choiceA;
+var choiceB;
+var choiceC;
+var choiceD;
+//Make a var for each question so make each a var to be a string
+var questionOne ="";
+var questionTwo ="";
+var questionThree ="";
+var questionFour ="";
+var questionFive ="";
+var questionSix ="";
+var questionSeven ="";
+var questionEight ="";
+var questionNine ="";
+var questionTen ="";
+
+
+
+
+
+
+
+
+
+}
