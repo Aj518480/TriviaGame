741ea51ca58d513d7f1e13bdd633ad4dcee0b525
aj518480 <allisoncordovez@gmail.com>
2019-09-08 16:38:46 -0400

    pseudo coded the questions that will be going into the question variables and choices of the answer and the correct answers as a key on the bottom for future use

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,7 +23,7 @@ var userResults;
 
 // String will pop up if user gets question right ot wrong
 var thatsRight ="That's what she said"
-var thatsWrong ="You Ignorant Slut!"
+var thatsWrong ="Dwight,You Ignorant Slut!"
 
 //Going to need a timer for each question for about 15-20 seconds
 //Going to need link Jquery to Html for the paragrah for the questions and to the buttons for the answers
@@ -34,7 +34,27 @@ var thatsWrong ="You Ignorant Slut!"
 
 
 
+//Question1 "What is Gabe's Middle Name?"
+//Question2 "Who is the Office Notary?"
+//Question3 "What are Oscars brain foods?"
+//Question4 "What is the name of Toby's True Crime Podcast?"
+//Question5 "In Threat Level Midnight'how many times does Michael Scran say he is going to toss the coin to determine whether he take the mission from the president?'"
+//Question6 "What ingredient should one undercook to make the perfect Malone chili?"
+//Question7 "Dwight Schrute plays which of the following musical instruments?"
+//Question8 "Which character is NOT chosen for Michael's basketball team in their match against the warehouse?"
+//Question9 "What is the name of the Scanton Strangler?"
+//Question10 "What is the number of Kevin invents to compensate for his accounting errors?"
 
+//ChoicesQ1: Sam,Susan,Seth,Sharon Correct Answer:Susan
+//ChoicesQ2: Toby,Angela,Dwight,Oscar Correct Answer:Dwight
+//ChoicesQ3: Sushi & Craft Beer, Thai Food & Spanish Reds, Empandas & Guacamole, Pizza Rolls and Celery Correct Answer: Thai Food & Spanish Reds
+//ChoicesQ4: Toby Tells All, My Name is George, The Flederson Files, Stolen Innocence:The Scraton Strangler Correct Answer: The Flederson Files
+//ChoicesQ5: 15, Brushing his teeth, 7 , 22 Correct Answer: 7
+//ChoicesQ6: The Hamburger Meat, The Tomatoes, The Stock, The Onions Correct Answer: The Onions
+//ChoicesQ7: Harpschord, Piano,Banjo, Recorder Correct Answer: Recorder
+//ChoicesQ8: Stanley,Phyllis,Ryan,Oscar Correct Answer: Oscar
+//ChoicesQ9: Charles Walter Fink, Douglas Lyle Goggins,George Howard Skub, Albert Henry DeSalvo Correct Answer: George Howard Skub
+//ChoicesQ10: Thirtween,Slevin,Keleven,Dundereight Correct Answer: Keleven
 
 
 
