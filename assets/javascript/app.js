58964445de8b2600ba83adbc9c974d222a9cdccb
aj518480 <allisoncordovez@gmail.com>
2019-09-08 15:55:40 -0400

    Made variables for user results to be stored and make 2 variable strings depending on if the user chooses the correct or wrong answer. Also pseudo coded some upcoming functions and timers that will be needed and if statements that will be needed in the future

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,7 +4,9 @@ var choiceA;
 var choiceB;
 var choiceC;
 var choiceD;
+
 //Make a var for each question so make each a var to be a string
+// Need to figure out to connect the right answers to the question variable
 var questionOne ="";
 var questionTwo ="";
 var questionThree ="";
@@ -16,6 +18,18 @@ var questionEight ="";
 var questionNine ="";
 var questionTen ="";
 
+// Need a variable to track the users correct and wrong answers
+var userResults;
+
+// String will pop up if user gets question right ot wrong
+var thatsRight ="That's what she said"
+var thatsWrong ="You Ignorant Slut!"
+
+//Going to need a timer for each question for about 15-20 seconds
+//Going to need link Jquery to Html for the paragrah for the questions and to the buttons for the answers
+// Going to need a place to link the gifs for the correct and wrong gifs
+//Need to make an onclick function for when the user clicks on the buttons with if statements of === or not !==
+//make sure it is including the user is === to put including the gif make thats right! is within the timers time frame. results with go up 1
 
 
 
